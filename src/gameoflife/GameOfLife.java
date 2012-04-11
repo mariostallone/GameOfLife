f5db16ebb73fe59c88c7e4f9bfdb92c401d643dd
Mario Stallone <mario@Marios-Mac-Pro.local>
2012-04-11 23:41:35 +0530

    Initial Commit

---
+++ src/gameoflife/GameOfLife.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gameoflife;
+
+/**
+ *
+ * @author mario
+ */
+public class GameOfLife {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
